730a6536817485f528a48f56cc483aea357a5e42
VanessaNiculae <vanessa.alexandra.niculae@gmail.com>
2023-04-01 19:26:37 +0300

    must-create-branch-first-hehe

---
+++ src/main-page/group-info/description.jsx
@@ -0,0 +1,12 @@
+export const Description = () => {
+    return(
+        <div className={"container description"}>
+            <div className={"row description-title"}>
+                <p>Description</p>
+            </div>
+            <div className={"row description-text"}>
+                <p></p>
+            </div>
+        </div>
+    )
+}
--- src/main-page/index.jsx
+++ src/main-page/index.jsx
@@ -3,15 +3,17 @@ import {Name} from "./group-info/name";
 import {ContactInfo} from "./group-info/contact-info";
 import {SocialLinks} from "./group-info/social-links";
 import {FirstRowContainer} from "./group-info/first-row-container";
+import {Description} from "./group-info/description";
 
 export const App = () => {
   return(
-  <div className={'main-div container'}>
+  <div className={'main-page container'}>
     <Name/>
     <FirstRowContainer>
       <ContactInfo/>
       <SocialLinks/>
     </FirstRowContainer>
+    <Description/>
   </div>
   )
 }
