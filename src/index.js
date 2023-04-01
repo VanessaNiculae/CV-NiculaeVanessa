dc5779aaad019581ebbd3970fac20c2f37d0b51c
VanessaNiculae <vanessa.alexandra.niculae@gmail.com>
2023-04-01 19:41:33 +0300

    styling-redone

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './style.css';
 import {App} from './main-page';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
