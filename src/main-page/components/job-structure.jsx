8fd36e5aef4fe0242aaeb9e56028d861e3081039
VanessaNiculae <vanessa.alexandra.niculae@gmail.com>
2023-04-02 13:44:49 +0300

    fixed-some-css-issues

--- src/main-page/components/job-structure.jsx
+++ src/main-page/components/job-structure.jsx
@@ -3,7 +3,7 @@ import "./style.css"
 export const JobStructure = ({job}) => {
     return (
         <div className={"job-structure"}>
-            <h1>{job.title}</h1>
+            <p className={"job-title"}>{job.title}</p>
             <p className={"job-company"}>{job.company}</p>
             <p className={"job-location"}>{job.location}</p>
             <p className={"job-timeperiod"}>{job.time_period}</p>
